3bf3d98ca549e3e1b612848a2da15829a95c9334
Michael Van Der Poll <michaelvdp@gmail.com>
2023-02-05 14:36:41 +0100

    adds midjourney art

--- Assets/Scripts/TreeReveal.cs
+++ Assets/Scripts/TreeReveal.cs
@@ -5,10 +5,17 @@ public class TreeReveal : MonoBehaviour
 {
     [SerializeField] private float treeDesiredHeight;
     [SerializeField] private float revealTime;
+    [SerializeField] private AudioSource source;
 
     [ContextMenu("Reveal")]
     public void Reveal()
     {
+        if (source != null)
+        {
+            source.loop = false;
+            source.Play();
+        }
+        
         transform.DOMoveY(treeDesiredHeight, revealTime);
     }
 }
