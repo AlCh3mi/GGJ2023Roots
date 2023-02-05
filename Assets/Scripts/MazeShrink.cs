61d901d8b7334662cb22d5fdef36a82461ee1566
Michael Van Der Poll <michaelvdp@gmail.com>
2023-02-05 13:38:15 +0100

    adds ShrinkMaze

---
+++ Assets/Scripts/MazeShrink.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class MazeShrink : MonoBehaviour
+{
+    [SerializeField] private Transform[] thingsToShrink;
+    [SerializeField] private float shrinkTime;
+    
+    [ContextMenu("Shrink")]
+    public void Shrink()
+    {
+        var parent = transform;
+        foreach (var tf in thingsToShrink)
+        {
+            tf.SetParent(parent);
+        }
+        transform.DOScale(new Vector3(1,0,1), shrinkTime).OnComplete(() =>
+        {
+            parent.gameObject.SetActive(false);
+        });
+    }
+}
