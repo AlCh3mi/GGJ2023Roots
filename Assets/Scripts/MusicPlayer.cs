6f47b52d6ff55891960eff69585897c36d67671d
Michael Van Der Poll <michaelvdp@gmail.com>
2023-02-05 13:04:55 +0100

    Adds Music Player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip music;
+
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic(music, true);
+    }
+}
