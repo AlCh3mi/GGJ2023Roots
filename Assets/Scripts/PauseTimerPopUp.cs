61c7875a9f87bce048e6b4eaaad054a4b4b6a426
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-06 06:05:50 +0100

    Make PauseTimePopUp rotate

--- Assets/Scripts/PauseTimerPopUp.cs
+++ Assets/Scripts/PauseTimerPopUp.cs
@@ -1,13 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using RoboRyanTron.Events;
 using UnityEngine;
-using StarterAssets;
 
 public class PauseTimerPopUp : MonoBehaviour
 {
     [SerializeField] private HourGlassTimer hourGlassTimer;
+    [SerializeField] private float rotationSpeed = 0.5f; // added a rotation speed variable
+
+    private void Update()
+    {
+        Debug.Log("Update function called");
+        transform.Rotate(0f, rotationSpeed, 0f); // rotate the game object each frame
+    }
 
     public void PauseTime()
     {
