cb0e29c9e29dc492ff8c059420b16a455eadb319
Michael Van Der Poll <michaelvdp@gmail.com>
2023-02-12 12:49:17 +0100

    Adds collectible UI icons for seed, water, shovel
    fixes credit screen quitting game, now it returns to start page

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -5,18 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float displayTime = 30f;
+    
+    IEnumerator Start()
     {
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        new WaitForSeconds(30);
-        Debug.Log("thanks for playing");
-        Application.Quit();
+        yield return new WaitForSeconds(displayTime);
+        SceneManager.LoadScene("StartPage");
     }
 }
---
+++ Assets/Scripts/UserInterface/CollectableUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RoboRyanTron.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    public class CollectableUI : GameEventListener
+    {
+        [SerializeField] private Color activeColor;
+        [SerializeField] private Color inactiveColor;
+
+        [SerializeField] private Image image;
+
+        private void Start()
+        {
+            SetColorActive(false);
+        }
+
+        public void SetColorActive(bool active)
+        {
+            image.color = active
+                ? activeColor
+                : inactiveColor;
+        }
+    }
+}
