f409522c8abaf1d918a3c93088396e0d3ec50cd4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-14 06:25:14 +0100

    Create CursorLock added to player

---
+++ Assets/Scripts/CursorLock.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour
+{
+    public KeyCode toggleKey = KeyCode.Escape;
+    public GameObject player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+
+        if (Cursor.lockState != CursorLockMode.Locked) return;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        player.transform.Rotate(Vector3.up * mouseX);
+        player.transform.Rotate(Vector3.right * -mouseY);
+    }
+}
